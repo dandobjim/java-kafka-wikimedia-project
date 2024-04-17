2c8921e28ed244b612d42316e7720e739ace3397
Daniel Doblas <ddoblas@theworkshop.com>
2024-04-17 14:53:47 +0200

    Update consumer, make it idempotent

--- kafka-consumer-opensearch/src/main/java/io/ddoblas/demos/kafka/opensearch/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/io/ddoblas/demos/kafka/opensearch/OpenSearchConsumer.java
@@ -1,5 +1,6 @@
 package io.ddoblas.demos.kafka.opensearch;
 
+import com.google.gson.JsonParser;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -31,7 +32,7 @@ import java.util.Properties;
 public class OpenSearchConsumer {
     private static final Logger log = LoggerFactory.getLogger(OpenSearchConsumer.class);
 
-    private static RestHighLevelClient createOpenSearchClient(){
+    private static RestHighLevelClient createOpenSearchClient() {
         String connString = "http://localhost:9200";
 
         //we build a URI from the connection string
@@ -40,9 +41,9 @@ public class OpenSearchConsumer {
         // extract login information if exists
         String userInfo = connUri.getUserInfo();
 
-        if(userInfo == null){
+        if (userInfo == null) {
             // REST client without security
-            restHighLevelClient = new RestHighLevelClient(RestClient.builder(new HttpHost(connUri.getHost(),connUri.getPort())));
+            restHighLevelClient = new RestHighLevelClient(RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort())));
         } else {
             // REST client with security
             String[] auth = userInfo.split(":");
@@ -51,7 +52,7 @@ public class OpenSearchConsumer {
             cp.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(auth[0], auth[1]));
 
             restHighLevelClient = new RestHighLevelClient(
-                    RestClient.builder(new HttpHost(connUri.getHost(),connUri.getPort(), connUri.getScheme()))
+                    RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), connUri.getScheme()))
                             .setHttpClientConfigCallback(
                                     httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(cp)
                                             .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
@@ -62,24 +63,34 @@ public class OpenSearchConsumer {
         return restHighLevelClient;
     }
 
-    private static KafkaConsumer createKafkaConsumer(){
+    private static KafkaConsumer createKafkaConsumer() {
 
         String bootstrapServers = "127.0.0.1:9092";
         String groupId = "consumer-opensearch-demo";
 
         // create consumer configs
         Properties props = new Properties();
-        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServers);
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG,groupId);
-        props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"latest");
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
 
         // create consumer
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
 
 
         return consumer;
+    }
+
+    private static String extractId(String json) {
+        // gson library
+        return JsonParser.parseString(json)
+                .getAsJsonObject()
+                .get("meta")
+                .getAsJsonObject()
+                .get("id")
+                .getAsString();
     }
 
     public static void main(String[] args) throws IOException {
@@ -92,44 +103,52 @@ public class OpenSearchConsumer {
         KafkaConsumer<String, String> consumer = createKafkaConsumer();
 
         // create index on OpenSearch if does not exist
-        try(openSearchClient; consumer) {
+        try (openSearchClient; consumer) {
 
             boolean indexExistes = openSearchClient.indices().exists(new GetIndexRequest("wikimedia"), RequestOptions.DEFAULT);
 
-            if(!indexExistes) {
+            if (!indexExistes) {
                 CreateIndexRequest createIndexRequest = new CreateIndexRequest("wikimedia");
                 openSearchClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
 
                 logger.info("The wikimedia index has been created");
-            }else {
+            } else {
                 logger.info("The wikimedia index already exists");
             }
 
             consumer.subscribe(Collections.singleton("wikimedia.recentchanges"));
 
-            while (true){
+            while (true) {
                 ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(3000));
 
                 int recordCount = records.count();
                 logger.info("Received " + recordCount + " records");
 
-                try {
-                    for (ConsumerRecord<String, String> record : records) {
-                        // send the record into opensearch
+                for (ConsumerRecord<String, String> record : records) {
+                    // send the record into opensearch
+
+                    // strategy 1
+                    // define an Id using Kafka Record coordinates
+                    // String id = record.topic() + "_" + record.partition() + "_" + record.offset();
+
+
+                    try {
+                        // strategy 2 - Extract id from JSON
+                        String id = extractId(record.value());
                         IndexRequest indexRequest = new IndexRequest("wikimedia")
-                                .source(record.value(), XContentType.JSON);
+                                .source(record.value(), XContentType.JSON)
+                                .id(id);
 
                         IndexResponse indexResponse = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
 
                         log.info(indexResponse.getId());
-                    }
-                } catch (Exception e) {
+                    } catch (Exception e) {
 
+                    }
                 }
 
             }
         }
-
 
 
         // main code logic
